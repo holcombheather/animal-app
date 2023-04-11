a1bf6bf22077b66c61b2b6e2745f146259684923
Heather Holcomb <holcombheather@gmail.com>
2023-04-10 22:38:44 -0700

    removed unnecessary stuff in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Header from './Header';
@@ -9,20 +8,9 @@ import Footer from './Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Horned animals 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
