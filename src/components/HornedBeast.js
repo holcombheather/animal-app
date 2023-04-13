b5d6660a3a9be45d9414404e74f83b61078152a3
Heather Holcomb <holcombheather@gmail.com>
2023-04-12 17:25:19 -0700

    validated onclick function and updated bootstrap formatting

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
 import { Card } from 'react-bootstrap';
+import CardHeader from 'react-bootstrap/esm/CardHeader';
 
 class HornedBeast extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = () => {
+        let addClick = this.state.timesClicked + 1;
+        this.setState({
+            timesClicked: addClick
+        })
+    }
+
     render() {
-        const { title, image_url, description } = this.props;
 
         return (
-            <Card className='HornedBeast'>
-                <Card.Img src={image_url} alt={title} title={title} />
+            <Card class='card' onClick={this.handleClick}>
+                <CardHeader class='card-header'>{this.state.timesClicked}</CardHeader>
+                <Card.Img src={this.props.image_url} alt={this.props.title} title={this.props.title} class='card-img-top'/>
+               
+                <Card.Title>{this.props.title}</Card.Title>
+               
                 <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>{description}</Card.Text>
+                    <Card.Text>{this.props.description}</Card.Text>
                 </Card.Body>
+
             </Card>
         )
     }
 }
 
 export default HornedBeast;
+
+
+// **DEMO code for reference**
+// class HornedBeast extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             "status": "Yay"
+//         }
+//     }
+
+//     handleClick = () => {
+//         const newStatus = this.state.status == "Nay" ? "Yay" : "Nay";
+//         this.setState({
+//             status: newStatus
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div onClick={this.handleClick}>
+//                 <Image src={this.props.image_url} alt="some horned beast" rounded flued />
+//                 <h3>{this.state.status}</h3>
+//             </div>
+//         )
+//     }
+// }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,129 +1,38 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
-// import { Row } from 'react-bootstrap';
-// import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 // import { Image } from 'react-bootstrap';
 import HornedBeast from './HornedBeast';
-// import imageUrls from './data.json';
+// import imageUrls from '../data.json';
 
 class Main extends Component {
+   
     render() {
 
         const imageUrls = this.props.imageUrls;
 
         return (
-            <Container>
+            <Container className='my-5'> 
                 <h2>Main Component</h2>
-                {imageUrls.map((beast, index) => (
-                    <HornedBeast 
-                        key={beast._id}
-                        title={beast.title}
-                        image_url={beast.image_url}
-                        description={beast.description}
-                    />
-                ))}
+                <Row className='g-4'>
+                {imageUrls.map((beast, index) => {
+                    return (
+                        <Col xs={12} md={6} lg={4}>
+                        <HornedBeast 
+                            key={beast._id}
+                            title={beast.title}
+                            image_url={beast.image_url}
+                            description={beast.description}
+                        />   
+                        </Col>
+                    )
+                })
+                }      
+                </Row>
              </Container>   
-            //     <Container>
-            //         <Row>
-            //             <Col>
-            //                 <HornedBeast title={this.props.title} image_url={beastImages[0].image_url} description={this.props.description} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast title={this.props.title} image_url={beastImages[1].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast title={this.props.title} image_url={beastImages[2].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast title={this.props.title} image_url={beastImages[3].image_url} />
-            //             </Col>
-            //         </Row>
-            //         <Row>
-            //             <Col>
-            //                 <HornedBeast title={this.props.title} image_url={beastImages[4].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast title={this.props.title} image_url={beastImages[5].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[6].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[7].image_url} />
-            //             </Col>
-            //         </Row>
-            //         <Row>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[8].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[9].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[10].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[11].image_url} />
-            //             </Col>
-            //         </Row>
-            //         <Row>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[12].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[13].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[14].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[15].image_url} />
-            //             </Col>
-            //         </Row>
-            //         <Row>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[16].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[17].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[18].image_url} />
-            //             </Col>
-            //             <Col>
-            //                 <HornedBeast image_url={beastImages[19].image_url} />
-            //             </Col>
-            //         </Row>
-            //     </Container>
-
-            // </div>
         );
     }
 }
 
 export default Main;
-
-// class HornedBeast extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             "status": "Yay"
-//         }
-//     }
-
-//     handleClick = () => {
-//         const newStatus = this.state.status == "Nay" ? "Yay" : "Nay";
-//         this.setState({
-//             status: newStatus
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <div onClick={this.handleClick}>
-//                 <Image src={this.props.image_url} alt="some horned beast" rounded flued />
-//                 <h3>{this.state.status}</h3>
-//             </div>
-//         )
-//     }
-// }
